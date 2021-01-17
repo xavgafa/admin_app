e39e8b8398162bf1cb33bb357b068cbe0b0f8191
Xavier Gaya <xavier.gaya@estudiantat.upc.edu>
2021-01-17 13:29:16 +0100

    push total amb feina de la Georgina inclosa

--- lib/parts/activitatsActuals.dart
+++ lib/parts/activitatsActuals.dart
@@ -5,17 +5,17 @@ class ActivitatsActuals extends StatefulWidget {
   const ActivitatsActuals({
     Key key,
   }) : super(key: key);
-
+ 
   @override
   _ActivitatsActualsState createState() => _ActivitatsActualsState();
 }
-
+ 
 class _ActivitatsActualsState extends State<ActivitatsActuals> {
   TextEditingController _day;
   TextEditingController _month;
   TextEditingController _year;
   final int dia = 18, month = 1, year = 2021;
-
+ 
   @override
   void initState() {
     _day = TextEditingController(text: '$dia');
@@ -23,93 +23,83 @@ class _ActivitatsActualsState extends State<ActivitatsActuals> {
     _year = TextEditingController(text: '$year');
     super.initState();
   }
-
+ 
   @override
   void dispose() {
     _day = TextEditingController();
     _month = TextEditingController();
     super.dispose();
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Container(
       //height: 700,
       width: 400,
-      color: Colors.blue[100],
-      /*child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              //mainAxisSize: MainAxisSize.max,
-              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Llista Activitats',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black,
-                  ),
-                ),
-                /*IconButton(
-                  icon: Icon(Icons.delete, color: Colors.black),
-                  onPressed: () {},
-                ),*/
-              ],
-            ),
-            /*SizedBox(height: 10),
-            Row(
-              //mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 60,
-                  height: 50,
-                  //color: Colors.blue,
-                  child: TextField(
-                    controller: _day,
-                    decoration: InputDecoration(
-                      labelText: 'Dia',
-                    ),
-                  ),
-                ),
-                SizedBox(width: 30),
-                Container(
-                  width: 60,
-                  height: 50,
-                  //color: Colors.blue,
-                  child: TextField(
-                    controller: _month,
-                    decoration: InputDecoration(
-                      labelText: 'Mes',
-                    ),
-                  ),
-                ),
-                SizedBox(width: 30),
-                Container(
-                  width: 60,
-                  height: 50,
-                  //color: Colors.blue,
-                  child: TextField(
-                    controller: _year,
-                    decoration: InputDecoration(
-                      labelText: 'Any',
-                    ),
-                  ),
-                ),
-                /*TextField(
-                  controller: _day,
-                  decoration: InputDecoration(labelText: 'Dia'),
-                ),*/
-              ],
-            ),*/
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Llistat Activitats'),
+          actions: [
+            //nomes eliminar les seleccionades
+            IconButton(
+              icon: Icon(Icons.delete, color: Colors.white),
+              onPressed: () {},
+            )
           ],
         ),
-      ),*/
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 60,
+                    height: 50,
+                    child: TextField(
+                      controller: _day,
+                      decoration: InputDecoration(
+                        labelText: 'Dia',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    width: 60,
+                    height: 50,
+                    child: TextField(
+                      controller: _month,
+                      decoration: InputDecoration(
+                        labelText: 'Mes',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    width: 60,
+                    height: 50,
+                    child: TextField(
+                      controller: _year,
+                      decoration: InputDecoration(
+                        labelText: 'Any',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              //AQUÍ VA EL LISTVIEW AMB EL LLISTAT D'ACTIVITATS
+              Expanded(
+                child: Container(
+                  color: Colors.blue[100],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
--- lib/parts/dadesActivitat.dart
+++ lib/parts/dadesActivitat.dart
@@ -4,64 +4,189 @@ class DadesActivitat extends StatefulWidget {
   const DadesActivitat({
     Key key,
   }) : super(key: key);
-
+ 
   @override
   _DadesActivitatState createState() => _DadesActivitatState();
 }
-
+ 
 class _DadesActivitatState extends State<DadesActivitat> {
-  TextEditingController controlTipus;
-  TextEditingController controlEntrenador;
-  TextEditingController controlSala;
-  TextEditingController controlAssis;
-  TextEditingController controlDia;
-  TextEditingController controlMes;
-  TextEditingController controlAny;
-  TextEditingController controlHoraInici;
-  TextEditingController controlMinInici;
-  TextEditingController controlHoraFinal;
-  TextEditingController controlMinFinal;
-
+  TextEditingController _controlTipus;
+  TextEditingController _controlEntrenador;
+  TextEditingController _controlSala;
+  TextEditingController _controlAssis;
+  TextEditingController _controlMaxAssis;
+  TextEditingController _controlDia;
+  TextEditingController _controlMes;
+  TextEditingController _controlAny;
+  TextEditingController _controlHoraInici;
+  TextEditingController _controlHoraFinal;
+ 
   @override
   void initState() {
-    controlTipus = TextEditingController();
-    controlEntrenador = TextEditingController();
-    controlSala = TextEditingController();
-    controlAssis = TextEditingController();
-    controlDia = TextEditingController();
-    controlMes = TextEditingController();
-    controlAny = TextEditingController();
-    controlHoraInici = TextEditingController();
-    controlMinInici = TextEditingController();
-    controlHoraFinal = TextEditingController();
-    controlMinFinal = TextEditingController();
-    // TODO: implement initState
+    _controlTipus = TextEditingController();
+    _controlEntrenador = TextEditingController();
+    _controlSala = TextEditingController();
+    _controlAssis = TextEditingController();
+    _controlMaxAssis = TextEditingController();
+    _controlDia = TextEditingController();
+    _controlMes = TextEditingController();
+    _controlAny = TextEditingController();
+    _controlHoraInici = TextEditingController();
+    _controlHoraFinal = TextEditingController();
     super.initState();
   }
-
+ 
   @override
   void dispose() {
-    controlTipus.dispose();
-    controlEntrenador.dispose();
-    controlSala.dispose();
-    controlAssis.dispose();
-    controlDia.dispose();
-    controlMes.dispose();
-    controlAny.dispose();
-    controlHoraInici.dispose();
-    controlMinInici.dispose();
-    controlHoraFinal.dispose();
-    controlMinFinal.dispose();
-    // TODO: implement dispose
+    _controlTipus.dispose();
+    _controlEntrenador.dispose();
+    _controlSala.dispose();
+    _controlAssis.dispose();
+    _controlMaxAssis.dispose();
+    _controlDia.dispose();
+    _controlMes.dispose();
+    _controlAny.dispose();
+    _controlHoraInici.dispose();
+    _controlHoraFinal.dispose();
     super.dispose();
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Container(
-      //height: 200,
+      width: 400,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Editar Activitats"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 200,
+                height: 60,
+                child: TextField(
+                  controller: _controlTipus,
+                  decoration: InputDecoration(labelText: 'Tipus Activitat'),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 200,
+                height: 60,
+                child: TextField(
+                  controller: _controlEntrenador,
+                  decoration: InputDecoration(labelText: 'Entrenador'),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 200,
+                height: 60,
+                child: TextField(
+                  controller: _controlSala,
+                  decoration: InputDecoration(labelText: 'Sala'),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 60,
+                    child: TextField(
+                      controller: _controlAssis,
+                      decoration: InputDecoration(labelText: 'Assistents'),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    width: 150,
+                    height: 60,
+                    child: TextField(
+                      controller: _controlMaxAssis,
+                      decoration:
+                          InputDecoration(labelText: 'Màxim assistents'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 60,
+                    child: TextField(
+                      controller: _controlDia,
+                      decoration: InputDecoration(labelText: 'Dia'),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    width: 50,
+                    height: 60,
+                    child: TextField(
+                      controller: _controlMes,
+                      decoration: InputDecoration(labelText: 'Mes'),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    width: 50,
+                    height: 60,
+                    child: TextField(
+                      controller: _controlAny,
+                      decoration: InputDecoration(labelText: 'Any'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 60,
+                    child: TextField(
+                      controller: _controlHoraInici,
+                      decoration: InputDecoration(labelText: 'Hora Inici'),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    width: 100,
+                    height: 60,
+                    child: TextField(
+                      controller: _controlHoraFinal,
+                      decoration: InputDecoration(labelText: 'Hora Final'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+ 
+    /*Container(
       width: 400,
       color: Colors.green[400],
+      child: Column(
+        children: [
+          Container(
+            width: 100,
+            height: 60,
+            child: TextField(
+              controller: _controlTipus,
+              decoration: InputDecoration(labelText: 'Tipus Activitat'),
+            ),
+          )
+        ],
+      ),
       /*child: Column(children: [
         TextField(
           decoration: InputDecoration(labelText: 'Tipus Activitat'),
@@ -155,7 +280,6 @@ class _DadesActivitatState extends State<DadesActivitat> {
           ],
         )
       ]),*/
-    );
+    );*/
   }
 }
-
